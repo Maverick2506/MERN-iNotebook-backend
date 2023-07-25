3012347fdfe286c5dbb4af8ebd421834aa857013
Maverick2506 <122738707+Maverick2506@users.noreply.github.com>
2023-07-25 18:58:03 -0400

    Created a db.js file which includes the mongo URI, created example models for user and notes, created example routes for authentication and notes

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://127.0.0.1";
+
+const connectToMongo = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(mongoURI);
+    console.log("Connected to Mongo Successfully!");
+  } catch (error) {
+  }
+};
+
+module.exports = connectToMongo;
