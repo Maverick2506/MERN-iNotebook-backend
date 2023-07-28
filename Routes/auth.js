7f83837e10c75264a3c561292b995ba8e13f614f
Maverick2506 <122738707+Maverick2506@users.noreply.github.com>
2023-07-28 15:05:42 -0400

    Encrypted the password to store in the data base with hash and salt and return authtoken to user and created a login endpoint which takes in email and password, verifies it with the database and returns authtoken

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../Models/User");
-const { body, validationResult, Result } = require("express-validator");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
+const JWT_SECRET = process.env.JWT_SIGN;
 
-// Create a User using: POST "/api/auth/createuser". Doesn't require Auth
+// Create a User using: POST "/api/auth/createuser". No login required
 router.post(
   "/createuser",
   [
     body("name", "Enter a valid name with aleast 3 letters!!").isLength({
       min: 3,
     }),
-    body("email", "Enter a valid email with @ and .com!!").isEmail(),
+    body("email", "Enter a valid email!!").isEmail(),
     body(
       "password",
       "Enter a valid password with minimum length of 8 characters!!!"
@@ -30,16 +34,75 @@ router.post(
           error: "Sorry! A user with this email address exists already.",
         });
       }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPwd = await bcrypt.hash(req.body.password, salt);
+
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPwd,
       });
 
-      res.json(user);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some error may have been occured");
+      res.status(500).send("Internal Server error may have occured");
+    }
+  }
+);
+
+// Authenticate User using: POST "/api/auth/login". Doesn't require Auth
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email!!").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //If there are errors return Bad request and the error message
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Invalid Credentials. Please check your email!!" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Invalid Credentials. Please check your password!!" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authtoken = jwt.sign(payload, JWT_SECRET);
+
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server error may have occured");
     }
   }
 );
