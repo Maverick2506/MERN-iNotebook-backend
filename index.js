b9f4678056cc2c1c5ef979e57de05030b336ef0e
Maverick2506 <122738707+Maverick2506@users.noreply.github.com>
2023-08-02 19:31:56 -0400

    Added cors in index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const connectToMongo = require("./db");
-
-connectToMongo();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8000;
+
+connectToMongo();
+
+app.use(cors());
 
 app.use(express.json());
 
